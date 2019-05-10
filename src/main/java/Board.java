076eca2ba2d4011cee4e186cb4bd8f2092a9d035
Tola's Terminal <sohbaker@gmail.com>
2019-05-10 14:25:21 +0100

    refactor

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Board {
     public List grid = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-    public static final int[][] allWinningCombinations = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {2, 4, 6}, {0, 4, 8} };
+    public static final int[][] allWinningCombinations = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {2, 4, 6}, {0, 4, 8}};
 
     public Boolean isEmpty() {
         return !grid.contains("X") && !grid.contains("O");
@@ -30,10 +30,10 @@ public class Board {
         Integer countOccurenceOfMark = 0;
         for (int i = 0; i < allWinningCombinations.length; i++) {
             int[] singleCombination = allWinningCombinations[i];
-            if(mark.equals(grid.get(singleCombination[0])) ) countOccurenceOfMark++;
-            if(mark.equals(grid.get(singleCombination[1])) ) countOccurenceOfMark++;
-            if(mark.equals(grid.get(singleCombination[2])) ) countOccurenceOfMark++;
-            if(countOccurenceOfMark.equals(3) ) win = true;
+            for (int b = 0; b < singleCombination.length; b++) {
+                if (mark.equals(grid.get(singleCombination[b]))) countOccurenceOfMark++;
+                if (countOccurenceOfMark.equals(3)) win = true;
+            }
             countOccurenceOfMark = 0;
         }
         return win;
