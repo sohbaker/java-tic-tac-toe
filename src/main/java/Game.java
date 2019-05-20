5f873af5bb0b536762832036f6834dd149c3e86e
Tola's Terminal <sohbaker@gmail.com>
2019-05-20 14:28:40 +0100

    knows that the game can continue

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,22 @@
 public class Game {
+    private final Display display;
+    private final Player playerOne;
+    private final Player playerTwo;
+    private Board board;
+
+    public Game(Board currentBoard, Display currentDisplay, Player player1, Player player2) {
+        board = currentBoard;
+        display = currentDisplay;
+        playerOne = player1;
+        playerTwo = player2;
+    }
+
+    public boolean isOver() {
+        boolean over = false;
+
+        if (board.isFull() || board.playerHasWon(playerOne.getMark()) || board.playerHasWon(playerTwo.getMark())) {
+            over = true;
+        }
+        return over;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+
 public class GameTest {
+    private Board board = new Board();
+    private Display display = new Display();
+    private Player player1;
+    private Player player2;
+
+    @Test
+    public void knowsThatTheGameCanContinue() {
+        player1 = new Human("X", display);
+        player2 = new Human("O", display);
+        Game game = new Game(board, display, player1, player2);
+        assertFalse(game.isOver());
+    }
 }
