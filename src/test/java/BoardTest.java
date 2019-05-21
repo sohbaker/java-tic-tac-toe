f764c0a168bf4d118353976915bf184719979471
Tola's Terminal <sohbaker@gmail.com>
2019-05-21 15:27:51 +0100

    change testing scenarios

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -39,7 +39,9 @@ public class BoardTest {
     @Test
     public void knowsThatAPlayerHasWon() {
         board.markBoard(1, "X");
+        board.markBoard(4, "O");
         board.markBoard(2, "X");
+        board.markBoard(5, "O");
         board.markBoard(3, "X");
         assertTrue(board.playerHasWon("X"));
     }
@@ -47,20 +49,20 @@ public class BoardTest {
     @Test
     public void knowsThatThePlayerHasNotWon() {
         board.markBoard(1, "X");
-        board.markBoard(2, "X");
+        board.markBoard(2, "O");
         assertFalse(board.playerHasWon("X"));
     }
 
     @Test
     public void knowsThatTheBoardIsFull() {
         board.markBoard(1, "X");
-        board.markBoard(2, "X");
+        board.markBoard(2, "O");
         board.markBoard(3, "X");
-        board.markBoard(4, "X");
+        board.markBoard(4, "O");
         board.markBoard(5, "X");
-        board.markBoard(6, "X");
+        board.markBoard(6, "O");
         board.markBoard(7, "X");
-        board.markBoard(8, "X");
+        board.markBoard(8, "O");
         board.markBoard(9, "X");
         assertTrue(board.isFull());
     }
@@ -68,7 +70,7 @@ public class BoardTest {
     @Test
     public void knowsThatTheBoardIsNotFull() {
         board.markBoard(1, "X");
-        board.markBoard(2, "X");
+        board.markBoard(2, "O");
         assertFalse(board.isFull());
     }
 
