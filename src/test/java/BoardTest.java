02b09fb332184b9650b486653623fb42d3be2b69
Tola's Terminal <sohbaker@gmail.com>
2019-05-21 15:23:04 +0100

    use before block

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -5,36 +6,38 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    private Board board;
+
+    @Before
+    public void setUpBoard() {
+        board = new Board();
+    }
+
     @Test
     public void newBoardIsEmpty() {
-        Board board = new Board();
         assertTrue(board.isEmpty());
     }
 
     @Test
     public void addAMarkToBoard() {
-        Board board = new Board();
         board.markBoard(1, "X");
         assertEquals("X", board.getCellAtPosition(1));
     }
 
     @Test
     public void knowsThatAMoveIsValid() {
-        Board board = new Board();
         board.markBoard(1, "X");
         assertTrue(board.isValidMove(2));
     }
 
     @Test
     public void knowsThatAMoveIsNotValid() {
-        Board board = new Board();
         board.markBoard(1, "X");
         assertFalse(board.isValidMove(1));
     }
 
     @Test
     public void knowsThatAPlayerHasWon() {
-        Board board = new Board();
         board.markBoard(1, "X");
         board.markBoard(2, "X");
         board.markBoard(3, "X");
@@ -43,7 +46,6 @@ public class BoardTest {
 
     @Test
     public void knowsThatThePlayerHasNotWon() {
-        Board board = new Board();
         board.markBoard(1, "X");
         board.markBoard(2, "X");
         assertFalse(board.playerHasWon("X"));
@@ -51,7 +53,6 @@ public class BoardTest {
 
     @Test
     public void knowsThatTheBoardIsFull() {
-        Board board = new Board();
         board.markBoard(1, "X");
         board.markBoard(2, "X");
         board.markBoard(3, "X");
@@ -66,7 +67,6 @@ public class BoardTest {
 
     @Test
     public void knowsThatTheBoardIsNotFull() {
-        Board board = new Board();
         board.markBoard(1, "X");
         board.markBoard(2, "X");
         assertFalse(board.isFull());
@@ -74,7 +74,6 @@ public class BoardTest {
 
     @Test
     public void knowsWhatCellsAreStillAvailable() {
-        Board board = new Board();
         board.markBoard(1, "X");
         List availableMoves = board.availableMoves();
         assertFalse(availableMoves.isEmpty());
@@ -82,20 +81,17 @@ public class BoardTest {
 
     @Test
     public void testReturnsTheMarkAtARequestedPosition() {
-        Board board = new Board();
         board.markBoard(1, "X");
         assertEquals("X", board.getCellAtPosition(1));
     }
 
     @Test
     public void testReturnsTheEmptyCellAtARequestedPosition() {
-        Board board = new Board();
         assertEquals("1", board.getCellAtPosition(1));
     }
 
     @Test
     public void returnsTheOpponentsMark() {
-        Board board = new Board();
         assertEquals("O", board.getOpponentMark("X"));
     }
 }
