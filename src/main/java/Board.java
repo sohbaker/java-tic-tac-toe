7f7cbfef0031695b470a865d23bf08e096d9e76c
Tola's Terminal <sohbaker@gmail.com>
2019-05-22 10:07:14 +0100

    tidy up

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,20 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Board {
     private final List grid = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-
     private static final int[][] WINNING_LINES = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {2, 4, 6}, {0, 4, 8}};
-
     private static final String[] PLAYER_MARKS = {"X", "O"};
 
     public boolean isEmpty() {
         return !grid.contains(PLAYER_MARKS[0]) && !grid.contains(PLAYER_MARKS[1]);
     }
 
+    public boolean isFull() {
+        return availableMoves().isEmpty();
+    }
+
     public void markBoard(int position, String mark) {
-       grid.set(position - 1, mark);
+        grid.set(position - 1, mark);
     }
 
     public boolean isValidMove(int position) {
@@ -26,21 +26,6 @@ public class Board {
         return playerHasWinningLine(mark);
     }
 
-    private boolean playerHasWinningLine(String mark) {
-        boolean win = false;
-        for (int i = 0; i < WINNING_LINES.length; i++) {
-            int[] singleLine = WINNING_LINES[i];
-            win = Arrays.stream(singleLine)
-                        .allMatch(x -> getCellAtPosition(x).equals(mark));
-            if (win) { break; }
-        }
-        return win;
-    }
-
-    public boolean isFull() {
-        return availableMoves().isEmpty();
-    }
-
     public List availableMoves() {
         List<Integer> availableCells = new ArrayList<>();
         for (int i = 0; i < grid.size(); i++) {
@@ -49,16 +34,12 @@ public class Board {
         return availableCells;
     }
 
-    private String getCellAtPosition(int i) {
-        return grid.get(i).toString();
-    }
-
     public String getOpponentMark(String mark) {
         String opponent = null;
 
         for (int i = 0; i < PLAYER_MARKS.length; i++) {
             if(PLAYER_MARKS[i] != mark) {
-              opponent = PLAYER_MARKS[i];
+                opponent = PLAYER_MARKS[i];
             }
         }
         return opponent;
@@ -74,11 +55,26 @@ public class Board {
     }
 
     public List gridCells() {
-       List<String> gridCells = new ArrayList<>();
+        List<String> gridCells = new ArrayList<>();
 
         for (int i = 0; i < grid.size(); i++) {
             gridCells.add(getCellAtPosition(i));
         }
         return gridCells;
+    }
+
+    private String getCellAtPosition(int i) {
+        return grid.get(i).toString();
+    }
+
+    private boolean playerHasWinningLine(String mark) {
+        boolean win = false;
+        for (int i = 0; i < WINNING_LINES.length; i++) {
+            int[] singleLine = WINNING_LINES[i];
+            win = Arrays.stream(singleLine)
+                    .allMatch(x -> getCellAtPosition(x).equals(mark));
+            if (win) { break; }
+        }
+        return win;
     }
 }
