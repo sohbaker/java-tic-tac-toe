601829451f8e62a62388f2ffca6ef42850873acd
Tola's Terminal <sohbaker@gmail.com>
2019-05-22 16:16:35 +0100

    create spy class. test game loop

---
+++ src/main/java/DisplaySpy.java
@@ -0,0 +1,37 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+
+class DisplaySpy extends Display {
+    private boolean win = false;
+    private boolean tie = false;
+
+    DisplaySpy(PrintStream console, InputStream input) {
+        super(console, input);
+    }
+
+    @Override
+    public void printGrid(Board board) {
+    }
+
+    @Override
+    public void promptPlayer(String mark) {
+    }
+
+    @Override
+    public void announceWinner(String mark) {
+        this.win = true;
+    }
+
+    @Override
+    public void announceTie() {
+        this.tie = true;
+    }
+
+    public boolean announceWinnerShouldHaveBeenCalled() {
+        return win;
+    }
+
+    public boolean announceTieShouldHaveBeenCalled() {
+        return tie;
+    }
+}
