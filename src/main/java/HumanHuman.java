bcfd7a965602e589890468408087d4244991a289
Tola's Terminal <sohbaker@gmail.com>
2019-05-31 09:41:21 +0100

    concrete class implements players interface

--- src/main/java/HumanHuman.java
+++ src/main/java/HumanHuman.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
-public class HumanHuman {
+public class HumanHuman implements Players {
+    @Override
+    public Human[] createPlayers(String playerOne, String playerTwo, Display display) {
+        Human obj[] = new Human[2];
+        obj[0] = new Human(playerOne, display);
+        obj[1] = new Human(playerTwo, display);
+        return obj;
+    }
 }
