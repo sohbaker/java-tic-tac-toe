814f2a2f40e23bbc5757a56aa92dbae15ccd2b9b
Tola's Terminal <sohbaker@gmail.com>
2019-06-10 10:23:26 +0100

    remove dead code

--- src/main/java/PlayersFactory.java
+++ src/main/java/PlayersFactory.java
@@ -1,16 +1,4 @@
 public class PlayersFactory {
- /*   public Players getPlayers(String gameType) {
-        if(gameType == null) {
-            return null;
-        }
-
-        if (gameType.equalsIgnoreCase("hh")) {
-            return new HumanHuman();
-        } else if (gameType.equalsIgnoreCase("hc")) {
-            return new HumanComputer();
-        }
-        return null;
-    }*/
     String[] types;
     Board board;
     Display display;
